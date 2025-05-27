1bf3e51edfe75d1afebfe7cc3548030ec11ab238
Peter Boden <peter.boden@team.blue>
2025-05-27 12:08:05 +0200

    build: fix build and ci
    - Use newer versions of checkout and upload-artifact actions
    - Install packer qemu plugin
    - Install correct qemu package
    - Install missing dependencies (libtirpc-dev, imagemagick)
    - Fix busybox compilation by adding CFLAGS and LDLIBS
    
    Signed-off-by: Peter Boden <peter.boden@team.blue>

--- src/busybox/build.sh
+++ src/busybox/build.sh
@@ -24,14 +24,15 @@ patch -Np1 -i ../busybox-1.33.0_skip-loop-control.patch
 patch -Np0 -i ../busybox-1.33.0_tc_depmod.patch
 
 cp -v ${REPODIR}/busybox-1.34.1_config_suid .config
-make CC="gcc -flto -mtune=generic -Os -pipe" CXX="g++ -flto -mtune=generic -Os -pipe -fno-exceptions -fno-rtti"
+
+make CC="gcc -flto -mtune=generic -Os -pipe" CXX="g++ -flto -mtune=generic -Os -pipe -fno-exceptions -fno-rtti" CFLAGS="-g -I/usr/include/tirpc" LDLIBS+="-lcrypt -lm -ltirpc"
 
 mkdir -p ${REPODIR}/pkg
-make CC="gcc -flto -mtune=generic -Os -pipe" CXX="g++ -flto -mtune=generic -Os -pipe -fno-exceptions -fno-rtti" CONFIG_PREFIX=${REPODIR}/pkg install
+make CC="gcc -flto -mtune=generic -Os -pipe" CXX="g++ -flto -mtune=generic -Os -pipe -fno-exceptions -fno-rtti" CFLAGS="-g -I/usr/include/tirpc" LDLIBS+="-lcrypt -lm -ltirpc" CONFIG_PREFIX=${REPODIR}/pkg install
 
 mv ${REPODIR}/pkg/bin/busybox ${REPODIR}/pkg/bin/busybox.suid
 chmod u+s ${REPODIR}/pkg/bin/busybox.suid
 
 cp -v ${REPODIR}/busybox-1.34.1_config_nosuid-patched .config
-make CC="gcc -flto -mtune=generic -Os -pipe" CXX="g++ -flto -mtune=generic -Os -pipe -fno-exceptions -fno-rtti"
-make CC="gcc -flto -mtune=generic -Os -pipe" CXX="g++ -flto -mtune=generic -Os -pipe -fno-exceptions -fno-rtti" CONFIG_PREFIX=${REPODIR}/pkg install
+make CC="gcc -flto -mtune=generic -Os -pipe" CXX="g++ -flto -mtune=generic -Os -pipe -fno-exceptions -fno-rtti" CFLAGS="-g -I/usr/include/tirpc" LDLIBS+="-lcrypt -lm -ltirpc"
+make CC="gcc -flto -mtune=generic -Os -pipe" CXX="g++ -flto -mtune=generic -Os -pipe -fno-exceptions -fno-rtti" CFLAGS="-g -I/usr/include/tirpc" LDLIBS+="-lcrypt -lm -ltirpc" CONFIG_PREFIX=${REPODIR}/pkg install
